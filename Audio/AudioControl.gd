a1d83f5a63aa3aeab8000d18641934436495a754
Mackenzie Beck <mack.beck977@gmail.com>
2026-01-31 14:35:16 -0700

    added autoloads

--- Audio/AudioControl.gd
+++ Audio/AudioControl.gd
@@ -1,5 +1,5 @@
 extends Node3D
-## Audio manager node. Inteded to be globally loaded as a 3D Scene. Handles [method create_2d_audio_at_location()] and [method create_audio()] to handle the playback and culling of simultaneous sound effects.
+## Audio manager node. Inteded to be globally loaded as a 2D Scene. Handles [method create_2d_audio_at_location()] and [method create_audio()] to handle the playback and culling of simultaneous sound effects.
 ##
 ## To properly use, define [enum SoundEffect.SOUND_EFFECT_TYPE] for each unique sound effect, create a Node3D scene for this AudioManager script add those SoundEffect resources to this globally loaded script's [member sound_effects], and setup your individual SoundEffect resources. Then, use [method create_3d_audio_at_location()] and [method create_audio()] to play those sound effects either at a specific location or globally.
 ## 

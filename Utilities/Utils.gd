2178ce96c5bf9b2e62c4c25f1841128544137ec1
Mackenzie Beck <mack.beck977@gmail.com>
2026-02-28 18:56:48 -0700

    added reference to main scene on utils

--- Utilities/Utils.gd
+++ Utilities/Utils.gd
@@ -9,9 +9,16 @@ var rng = RandomNumberGenerator.new()
 
 
 
+# can store a reference to the 'main' scene here, this would basically just be the parent node of the world grid and environment etc.
+# right now I have a direct path reference, so when we merge everything and decide where main goes this will have to be moved
+var main_scene = preload("uid://5klo3y5bav62")
 
 #flags
 # put global flags here, global flags are generally bad practice but they can be useful in a pinch, so if we must use them its good to have a central location to store them. 
+
+
+func set_main_scene(scene:Node2D):
+	main_scene = scene
 
 
 # Create a timer anywhere in the game tree and connect it to a funciton anywhere in teh game tree.
@@ -40,10 +47,18 @@ func save() -> void:
 	ResourceSaver.save(saved_game, "user://savegame.tres") # only one save file for now, in the future I can change this so that the user is prompted to enter a save name for multiple saves
 
 func load() -> void:
-	var saved_gmae:SavedGame = load("user://savegame.tres") as SavedGame
+	var saved_game:SavedGame = load("user://savegame.tres") as SavedGame
 	
 	get_tree().call_group("persist", "on_before_load_game") 
 	# call this function on every persistent node to do any prep work before loading game, ie. clearing enemies from a scene or resetting UI components
+	
+	for item in saved_game.saved_data:
+		var scene = load(item.scene_path) as PackedScene
+		var restored_node = scene.instantiate()
+		
+		if restored_node.has_method("on_load_game"):
+			restored_node.on_load_game(item)
+	
 	
 	#at this point I think I need to discuss with the team, the way our archietcture is set up all of the saved data should be collected and loaded by the combat_grid and player_manager
 	# however I think it may be better to have individual nodes in the combat_grid implement the sav/load contract instead, that should make it much easier to add new dynamic

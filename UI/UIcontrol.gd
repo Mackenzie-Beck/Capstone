58cd23be93f10dbcadf3d7aee12197c247c91547
ZachLap64 <zachlap64@gmail.com>
2026-03-10 12:58:56 -0600

    Main menu and fuel counter
    Setup main menu scene, no signals. Added fuel counter to player_control_ui.

--- UI/UIcontrol.gd
+++ UI/UIcontrol.gd
@@ -16,12 +16,14 @@ extends Control
 
 # All of the views should be added as children of this canvas layer. So they are all on the same 'level'
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
+@onready var main_menu: PanelContainer = $CanvasLayer/MainMenu
 @onready var pause_menu: PanelContainer = $CanvasLayer/PauseMenu
 @onready var player_control_ui: PlayerControlUI = $CanvasLayer/PlayerControlUI
 
 
 enum VIEWS {
 	PAUSE,
+	MAIN,
 	PLAYER
 }
 
@@ -41,4 +43,7 @@ func switch_view(view: VIEWS) -> void:
 		VIEWS.PLAYER:
 			hide_views()
 			player_control_ui.show()
+		VIEWS.MAIN:
+			hide_views()
+			main_menu.show()
 			

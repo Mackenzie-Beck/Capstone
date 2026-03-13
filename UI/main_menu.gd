54fe6c49ba818e634c371f4d7b5dbd156eee71cd
Mackenzie Beck <mack.beck977@gmail.com>
2026-03-13 11:51:39 -0600

    main menu buttons now wired up

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,12 +1,10 @@
 extends PanelContainer
 
-signal start_game()
-
 
 
 func _on_start_game_pressed() -> void:
-	emit_signal("start_game")
-	pass # Replace with function body.
+	SB.start_game.emit()
+
 
 
 func _on_quit_pressed() -> void:

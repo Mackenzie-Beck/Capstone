8b5124ec4cc89d49a3e2a82cb170d5d7046947dc
Mackenzie <mack.beck977@gmail.com>
2026-03-20 13:44:10 -0600

    bomb highlights no lonnger disappear when moused over

--- environment/object_layer.gd
+++ environment/object_layer.gd
@@ -4,30 +4,30 @@ extends TileMapLayer
 @export var player_sprite_atlas_coords : Vector2 
 @export var enemy_sprite_atlas_coords : Vector2 # set default values for these when assets are decided
 
-@export var player_coords : Vector2 = Vector2(0,0)
-@export var enemy_coords : Vector2 = Vector2(10,-10)
+@export var player_coords : Vector2 = Vector2i(0,0)
+@export var enemy_coords : Vector2 = Vector2i(10,-10)
 
-@export var tile_map_bounds : Vector2 = Vector2(40,40)
+@export var tile_map_bounds : Vector2 = Vector2i(40,40)
 
 @export var highlight_layer: TileMapLayer
-@export var highlight_atlas_coords: Vector2i = Vector2.ZERO
-@export var highlight_bomb_coordds: Vector2i = Vector2i(1,0)
+@export var highlight_atlas_coords: Vector2i = Vector2i.ZERO
+@export var highlight_bomb_coords: Vector2i = Vector2i(1,0)
 var last_hovered_tile: Vector2i = Vector2i(-1, -1)
 
 
 
 func _ready() -> void:
-	set_player_coords(Vector2(0,0))
-	set_enemy_coords(Vector2(5,5))
-	bomb(to_local(Vector2(3,3)))
+	set_player_coords(Vector2i(0,0))
+	set_enemy_coords(Vector2i(5,5))
+	bomb(Vector2i(3,3))
 
 # Will need to change the second arg of set_cell when the tilemap resource is created
-func set_player_coords(coords : Vector2):
+func set_player_coords(coords : Vector2i):
 	player_coords = coords
 	set_cell(player_coords, 2, player_sprite_atlas_coords)
 	
 	
-func set_enemy_coords(coords : Vector2):
+func set_enemy_coords(coords : Vector2i):
 	enemy_coords = coords
 	set_cell(enemy_coords,0,  enemy_sprite_atlas_coords)
 
@@ -38,11 +38,14 @@ func _process(_delta: float) -> void:
 	
 	if hovered_tile == last_hovered_tile:
 		return
+		
 
-	highlight_layer.erase_cell(last_hovered_tile)  # removes the single highlight cell
+	
+	if highlight_layer.get_cell_atlas_coords(last_hovered_tile) != highlight_bomb_coords:
+			highlight_layer.erase_cell(last_hovered_tile)
 
 	if hovered_tile.x >= -tile_map_bounds.x and hovered_tile.x < tile_map_bounds.x and \
-	hovered_tile.y >= -tile_map_bounds.y and hovered_tile.y < tile_map_bounds.y:
+	hovered_tile.y >= -tile_map_bounds.y and hovered_tile.y < tile_map_bounds.y and highlight_layer.get_cell_atlas_coords(hovered_tile) != highlight_bomb_coords:
 		highlight_layer.set_cell(hovered_tile, 0, highlight_atlas_coords)
 		last_hovered_tile = hovered_tile
 	else:
@@ -53,8 +56,12 @@ func _process(_delta: float) -> void:
 	
 
 
-func bomb(center_coord: Vector2) ->void:
+
+
+# a little annoying but the center_coord will be flipped on the grid
+#ie if you enter (3,3) the rendered point will be (-3,-3)
+func bomb(center_coord: Vector2i) ->void:
 	# Create highlight at coord, just start with a 3x3 area
 	for x in range(-1,2):
 		for y in range(-1,2):
-			highlight_layer.set_cell(Vector2(center_coord.x+x, center_coord.y+y), 0, highlight_bomb_coordds)
+			highlight_layer.set_cell(Vector2i(center_coord.x+x, center_coord.y+y), 0, highlight_bomb_coords)

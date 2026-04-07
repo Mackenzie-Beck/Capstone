fee5c4765a90df3554e119b6ac7c247833a9e6cc
ZachLap64 <zachlap64@gmail.com>
2026-04-07 15:03:01 -0600

    Update health_display.gd

--- UI/health_display.gd
+++ UI/health_display.gd
@@ -14,15 +14,18 @@ func _ready() -> void:
 	Player2Health.value = 10
 
 func update_player_hbar(val: int) -> void:
+	AudioControl.create_audio(SoundEffect.SOUND_EFFECT_TYPE.ROBO1)
 	if PlayerManager.multiplayer_check:
 		Player1HealthMulti.value = val
 	else:
 		Player1Health.value = val
 	
 func update_player2_hbar(val: int) -> void:
+	AudioControl.create_audio(SoundEffect.SOUND_EFFECT_TYPE.ROBO1)
 	Player2Health.value = val
 	
 func update_enemy_hbar(val: int) -> void:
+	AudioControl.create_audio(SoundEffect.SOUND_EFFECT_TYPE.ROBO4)
 	if PlayerManager.multiplayer_check:
 		EnemyHealthMulti.value = val
 	else:

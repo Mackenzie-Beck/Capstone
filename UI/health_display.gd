e612b8cd2f7db922b905d28642c637c09a9a09de
Mackenzie Beck <mack.beck977@gmail.com>
2026-04-26 20:18:17 -0600

    went back to default health display

--- UI/health_display.gd
+++ UI/health_display.gd
@@ -19,26 +19,31 @@ func _ready() -> void:
 
 
 func reset_health():
+	Player1Health.value = 10
+	EnemyHealth.value = 10
+	Player1HealthMulti.value = 10
+	EnemyHealthMulti.value = 10
+	Player2Health.value = 10
 	if PlayerManager.multiplayer_check:
-		# In multiplayer, read from both players
-		Player1HealthMulti.value = PlayerManager.player_array[0].health
-		Player2Health.value = PlayerManager.player_array[1].health
-		EnemyHealthMulti.value = Utils.main_scene.enemy.health
+		if PlayerManager.current_player == 0:
+			update_player_hbar(Player1HealthMulti.value)
+		elif PlayerManager.current_player == 1:
+			update_player2_hbar(Player2Health.value)
+		update_enemy_hbar(EnemyHealthMulti.value)
 	else:
-		# In single player, read from current player
-		Player1Health.value = PlayerManager.get_health()
-		EnemyHealth.value = Utils.main_scene.enemy.health
+		update_player_hbar(Player1Health.value)
+		update_enemy_hbar(EnemyHealth.value)
 
 func _on_player_health_update(value):
 	#print("_on_player_health_update in health_display")
 	AudioControl.create_audio(SoundEffect.SOUND_EFFECT_TYPE.ROBO1)
 	if PlayerManager.multiplayer_check:
 		if PlayerManager.current_player == 0:
-			update_player_hbar(PlayerManager.get_health() + value)
+			update_player_hbar(Player1HealthMulti.value+value)
 		elif PlayerManager.current_player == 1:
-			update_player2_hbar(PlayerManager.get_health() + value)
+			update_player2_hbar(Player2Health.value+value)
 	else:
-		update_player_hbar(PlayerManager.get_health() + value)
+		update_player_hbar(Player1Health.value+value)
 
 func update_player_hbar(val: int) -> void:
 	#print("update_player_hbar in health_display")
@@ -65,5 +70,9 @@ func update_enemy_hbar(val: int) -> void:
 		EnemyHealth.value = val
 
 func _on_game_loaded():
-	print("_on_game_loaded: Refreshing health display")
-	reset_health()
+	if PlayerManager.multiplayer_check:
+		update_player_hbar(PlayerManager.get_health())
+		update_enemy_hbar(Utils.main_scene.enemy.health)
+	else:
+		update_player_hbar(PlayerManager.get_health())
+		update_enemy_hbar(Utils.main_scene.enemy.health)
